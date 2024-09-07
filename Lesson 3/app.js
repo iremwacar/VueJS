289cf544ab135b9197674f0fe4c1ec245eefe9b1
irem acar <iremmacar35@gmail.com>
2024-09-07 15:25:41 +0300

    commit

---
+++ Lesson 3/app.js
@@ -0,0 +1,29 @@
+const app = Vue.createApp({
+    
+    data(){
+        return{
+            url:"www.youtube.com",
+            showBooks:true,
+            books: [
+                {title:'name of wind', author:'patric rothfuss', img:'assets/1.jpg', isFav:true},
+                {title:'the way of kings', author:'brandon sanderson', img:'assets/2.jpg', isFav:false},
+                {title:'the final empire', author:'brandon sanderson', img:'assets/3.jpg', isFav:true}
+            ]
+        }
+    },
+
+    methods: {
+        changeTitle() {
+            this.title = 'methods in title'
+        },
+        changeTitleName(title) {
+            this.title = title
+        },
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+        }
+    }
+
+})
+
+app.mount('#app')
